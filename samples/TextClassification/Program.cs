336676411665ea7c81a261ed13cb3000213ffcac
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2019-10-27 22:55:08 +0100

    improve language detection example

--- samples/TextClassification/Program.cs
+++ samples/TextClassification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace TextClassification
 {
     class Program
     {
+        private static ILogger Logger = ApplicationLogging.CreateLogger<Program>();
+
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -35,66 +38,63 @@ namespace TextClassification
 
             fastText.Data.Type = FastText.ModelType.Supervised;
             fastText.Data.Loss = FastText.LossType.OneVsAll;
-            fastText.Data.LearningRate = 0.7f;
-            fastText.Data.Dimensions = 128;
+            fastText.Data.LearningRate = 1f;
+            fastText.Data.Dimensions = 256;
             fastText.Data.Epoch = 100;
             fastText.Data.MinimumWordNgramsCounts = 5;
             fastText.Data.MaximumWordNgrams = 3;
+            fastText.Data.MinimumCount = 5;
 
             fastText.Train(trainDocs);
 
-
-            //Compute precision and Recall for the Train Set
-            float TP = 0f, FP = 0f, R = 0f, cutoff = 0.5f;
-
-            foreach (var doc in trainDocs)
+            //Compute predictions
+            Dictionary<IDocument, Dictionary<string, float>> predTrain, predTest;
+            using (new Measure(Logger, "Computing train-set predictions", trainDocs.Length))
             {
-                var pred = fastText.Predict(doc).Where(kv => kv.Value >cutoff).ToArray();
-                foreach (var p in pred)
-                {
-                    if (doc.Labels.Contains(p.Key))
-                    {
-                        TP++;
-                    }
-                    else
-                    {
-                        FP++;
-                    }
-                }
-
-                R += doc.Labels.Count;
+                predTrain = trainDocs.AsParallel().Select(d => (Doc: d, Pred: fastText.Predict(d))).ToDictionary(d => d.Doc, d => d.Pred);
             }
 
-            var precision = TP / (TP + FP);
-            var recall = TP / (R);
-
-            Console.WriteLine($"Train: F1={2 * (precision * recall) / (precision + recall):n2} P={precision:n2} R={recall:n2}");
-
-            //Compute precision and Recall for the Test Set
-            TP = 0f; FP = 0f; R = 0f;
-            foreach (var doc in testDocs)
+            using (new Measure(Logger, "Computing test set predictions", testDocs.Length))
             {
-                var pred = fastText.Predict(doc).Where(kv => kv.Value > cutoff).ToArray();
-                foreach(var p in pred)
-                {
-                    if (doc.Labels.Contains(p.Key))
-                    {
-                        TP++;
-                    }
-                    else
-                    {
-                        FP++;
-                    }
-                }
-
-                R += doc.Labels.Count;
+                predTest = testDocs.AsParallel().Select(d => (Doc: d, Pred: fastText.Predict(d))).ToDictionary(d => d.Doc, d => d.Pred);
             }
 
-            precision = TP / (TP + FP);
-            recall    = TP / (R);
+            var resultsTrain = ComputeStats(predTrain);
+            var resultsTest  = ComputeStats(predTest);
 
-            Console.WriteLine($"Test: F1={2*(precision*recall)/(precision+recall):n2} P={precision:n2} R={recall:n2}");
+            Console.WriteLine("\n\n\n--- Results ---\n\n\n");
+            foreach (var res in resultsTrain.Zip(resultsTest))
+            {
+                Console.WriteLine($"\tScore cutoff: {res.First.Cutoff:n2} Train: F1={res.First.F1:n2} P={res.First.Precision:n2} R={res.First.Recall:n2} Test: F1={res.Second.F1:n2} P={res.Second.Precision:n2} R={res.Second.Recall:n2}");
+            }
+
             Console.ReadLine();
+        }
+
+        private static (float Cutoff, float Precision, float Recall, float F1)[] ComputeStats(Dictionary<IDocument, Dictionary<string, float>> predictions)
+        {
+            return Enumerable.Range(40, 20)
+                             .Select(i => i / 100f) //Cutoff range: 0.4f to 0.6f
+                             .Select(cutoff =>
+                                (Cutoff: cutoff,
+                                Predictions:  predictions.Select(kv =>
+                                     {
+                                         return (TruePositive: kv.Key.Labels.Count(lbl => kv.Value.TryGetValue(lbl, out var score) && score > cutoff),
+                                                 FalsePositive: kv.Value.Count(pred => pred.Value > cutoff && !kv.Key.Labels.Contains(pred.Key)),
+                                                 Count: kv.Key.Labels.Count);
+                                     })
+                                     .ToArray()
+                                )
+                             )
+                             .Select(results =>
+                             {
+                                 var TP = (float)results.Predictions.Sum(r => r.TruePositive);
+                                 var FP = (float)results.Predictions.Sum(r => r.FalsePositive);
+                                 var precision = TP / (TP + FP);
+                                 var recall = TP / (results.Predictions.Sum(r => r.Count));
+                                 return (Cutoff: results.Cutoff, Precision: precision, Recall: recall, F1: 2 * (precision * recall) / (precision + recall));
+                             })
+                             .ToArray();
         }
     }
 }
