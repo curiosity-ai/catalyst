bf3536d867af4e1c05dd86ed338bad991df41b64
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2019-10-27 12:33:05 +0100

    explain text-classification sample

--- samples/TextClassification/Program.cs
+++ samples/TextClassification/Program.cs
@@ -20,13 +20,17 @@ namespace TextClassification
             //Configures the model storage to use the online repository backed by the local folder ./catalyst-models/
             Storage.Current = new OnlineRepositoryStorage(new DiskStorage("catalyst-models"));
 
+
+            //Download the Reuters corpus if necessary
             var (train, test) = await Corpus.Reuters.GetAsync();
 
+            //Parse the documents using the English pipeline, as the text data is untokenized so far
             var nlp = Pipeline.For(Language.English);
 
-            var trainDocs = nlp.Process(train);
-            var testDocs  = nlp.Process(test);
+            var trainDocs = nlp.Process(train).ToArray();
+            var testDocs  = nlp.Process(test).ToArray();
 
+            //Train a FastText supervised classifier with a multi-label loss (OneVsAll)
             var fastText = new FastText(Language.English, 0, "Reuters-Classifier");
 
             fastText.Data.Type = FastText.ModelType.Supervised;
@@ -39,9 +43,36 @@ namespace TextClassification
 
             fastText.Train(trainDocs);
 
+
+            //Compute precision and Recall for the Train Set
             float TP = 0f, FP = 0f, R = 0f, cutoff = 0.5f;
 
-            foreach(var doc in testDocs)
+            foreach (var doc in trainDocs)
+            {
+                var pred = fastText.Predict(doc).Where(kv => kv.Value >cutoff).ToArray();
+                foreach (var p in pred)
+                {
+                    if (doc.Labels.Contains(p.Key))
+                    {
+                        TP++;
+                    }
+                    else
+                    {
+                        FP++;
+                    }
+                }
+
+                R += doc.Labels.Count;
+            }
+
+            var precision = TP / (TP + FP);
+            var recall = TP / (R);
+
+            Console.WriteLine($"Train: F1={2 * (precision * recall) / (precision + recall):n2} P={precision:n2} R={recall:n2}");
+
+            //Compute precision and Recall for the Test Set
+            TP = 0f, FP = 0f, R = 0f,
+            foreach (var doc in testDocs)
             {
                 var pred = fastText.Predict(doc).Where(kv => kv.Value > cutoff).ToArray();
                 foreach(var p in pred)
@@ -59,8 +90,8 @@ namespace TextClassification
                 R += doc.Labels.Count;
             }
 
-            var precision = TP / (TP + FP);
-            var recall    = TP / (R);
+            precision = TP / (TP + FP);
+            recall    = TP / (R);
 
             Console.WriteLine($"Test: F1={2*(precision*recall)/(precision+recall):n2} P={precision:n2} R={recall:n2}");
             Console.ReadLine();
