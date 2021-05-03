1640b3e0e7897ec8eacc4f4ced3cc347e613df3f
rafael-aero <rafael@rafael.aero>
2021-05-03 16:37:36 +0200

    Update English.Map.cs

--- Languages/English/English.Map.cs
+++ Languages/English/English.Map.cs
@@ -8,15 +8,15 @@ namespace Catalyst.Models
 {
     public static partial class English
     {
-        public static sealed class Map
+        public static class Map
         {
             private static Lazy<Lookups> _lookup_us2uk = new Lazy<Lookups>(() => Lookups.FromStream(ResourceLoader.OpenResource(typeof(Lemmatizer).Assembly, "en_us2uk.bin")).WaitResult());
             private static Lazy<Lookups> _lookup_uk2us = new Lazy<Lookups>(() => Lookups.FromStream(ResourceLoader.OpenResource(typeof(Lemmatizer).Assembly, "en_uk2us.bin")).WaitResult());
 
-            public static string ToAmericanEnglish(IToken token) => new string(ToAmericanEnglishAsSpan(token));
-            public static string ToBritishEnglish(IToken token) => new string(ToBritishEnglishAsSpan(token));
-            public static ReadOnlySpan<char> ToAmericanEnglishAsSpan(IToken token) => _lookup_uk2us.Value.Get(token);
-            public static ReadOnlySpan<char> ToBritishEnglishAsSpan(IToken token) => _lookup_us2uk.Value.Get(token);
+            public static string ToAmerican(IToken token) => new string(ToAmericanAsSpan(token));
+            public static string ToBritish(IToken token) => new string(ToBritishAsSpan(token));
+            public static ReadOnlySpan<char> ToAmericanAsSpan(IToken token) => _lookup_uk2us.Value.Get(token);
+            public static ReadOnlySpan<char> ToBritishAsSpan(IToken token) => _lookup_us2uk.Value.Get(token);
         }
     }
 }
