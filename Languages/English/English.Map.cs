3aa93f6a6ead98687d316c3bf140dc410da19271
rafael-aero <rafael@rafael.aero>
2021-05-03 16:36:09 +0200

    change map class to static, clean up methods

--- Languages/English/English.Map.cs
+++ Languages/English/English.Map.cs
@@ -8,17 +8,15 @@ namespace Catalyst.Models
 {
     public static partial class English
     {
-        public sealed class Map
+        public static sealed class Map
         {
-            public Language Language => Language.English;
-            
             private static Lazy<Lookups> _lookup_us2uk = new Lazy<Lookups>(() => Lookups.FromStream(ResourceLoader.OpenResource(typeof(Lemmatizer).Assembly, "en_us2uk.bin")).WaitResult());
             private static Lazy<Lookups> _lookup_uk2us = new Lazy<Lookups>(() => Lookups.FromStream(ResourceLoader.OpenResource(typeof(Lemmatizer).Assembly, "en_uk2us.bin")).WaitResult());
 
-            public string GetLemma(IToken token) => new string(ToAmericanEnglish(token));
-            
-            public ReadOnlySpan<char> ToAmericanEnglish(IToken token) => _lookup_uk2us.Value.Get(token);
-            public ReadOnlySpan<char> ToBritishEnglish(IToken token) => _lookup_us2uk.Value.Get(token);
+            public static string ToAmericanEnglish(IToken token) => new string(ToAmericanEnglishAsSpan(token));
+            public static string ToBritishEnglish(IToken token) => new string(ToBritishEnglishAsSpan(token));
+            public static ReadOnlySpan<char> ToAmericanEnglishAsSpan(IToken token) => _lookup_uk2us.Value.Get(token);
+            public static ReadOnlySpan<char> ToBritishEnglishAsSpan(IToken token) => _lookup_us2uk.Value.Get(token);
         }
     }
 }
