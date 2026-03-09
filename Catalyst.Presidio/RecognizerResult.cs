6ea6f47db1f9692312fc1bf4996c8a2d53396d55
theolivenbaum <rafael@curiosity.ai>
2026-03-10 00:06:36 +0100

    Fix patterns

--- Catalyst.Presidio/RecognizerResult.cs
+++ Catalyst.Presidio/RecognizerResult.cs
@@ -5,31 +5,48 @@ namespace Catalyst.Presidio
     /// <summary>
     /// Represents the result of a PII recognition.
     /// </summary>
-    public class RecognizerResult
+    public struct RecognizerResult
     {
+        public RecognizerResult(int start, int end, string entityType, float score)
+        {
+            Start = start;
+            End = end;
+            EntityType = entityType;
+            Score = score;
+        }
+
         /// <summary>
         /// Gets or sets the start index of the identified PII (inclusive).
         /// </summary>
-        public int Start { get; set; }
+        public int Start { get; }
 
         /// <summary>
         /// Gets or sets the end index of the identified PII (inclusive).
         /// </summary>
-        public int End { get; set; }
+        public int End { get; }
 
         /// <summary>
         /// Gets or sets the confidence score of the recognition.
         /// </summary>
-        public double Score { get; set; }
+        public float Score { get; }
 
         /// <summary>
         /// Gets or sets the type of the identified PII (e.g., EMAIL_ADDRESS).
         /// </summary>
-        public string EntityType { get; set; }
+        public string EntityType { get; }
 
         /// <summary>
         /// Gets the length of the identified PII.
         /// </summary>
         public int Length => End - Start + 1;
+    }
+
+
+    public static class ITokensHelper
+    {
+        public static RecognizerResult AsRecognizerResult(this ITokens entity)
+        {
+            return new RecognizerResult(entity.Begin, entity.End, entity.EntityType.Type, 1.0f);
+        }
     }
 }
